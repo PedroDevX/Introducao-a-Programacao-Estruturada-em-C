ad6608b6e0d118a1b143d48de7bcf2b9aefca77e
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:36:48 -0300

    Update exercício010.c

--- c/iniciante/exercício010.c
+++ c/iniciante/exercício010.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main() {
+    int n1, n2;
+
+    printf("Digite dois números inteiros: ");
+    scanf("%d %d", &n1, &n2);
+
+    if (n1 > n2) {
+        printf("%d é maior que %d\n", n1, n2);
+    } else if (n2 > n1) {
+        printf("%d é maior que %d\n", n2, n1);
+    } else {
+        printf("Os dois números são iguais\n");
+    }
+
+    return 0;
+}
