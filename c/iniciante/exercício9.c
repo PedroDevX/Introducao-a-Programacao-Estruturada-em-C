43daaa3d0fa7bdb5d1a07b5f52d97dec86876002
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:15:20 -0300

    Update exercício9.c

--- c/iniciante/exercício9.c
+++ c/iniciante/exercício9.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main() {
+    int n1, n2, n3;
+    int resultado;
+
+    printf("Digite três números inteiros separados por espaço: ");
+    scanf("%d %d %d", &n1, &n2, &n3);
+
+    resultado = (n1 + n2) * n3;
+
+    printf("O resultado da expressão (n1 + n2) * n3 é: %d\n", resultado);
+
+    return 0;
+}
