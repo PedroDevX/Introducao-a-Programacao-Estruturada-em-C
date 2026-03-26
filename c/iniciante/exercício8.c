0ef85370a6c6111a2552f4bda46a1f01f85552e4
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:15:09 -0300

    Update exercício8.c

--- c/iniciante/exercício8.c
+++ c/iniciante/exercício8.c
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main() {
+    int numero;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    printf("O resto da divisão por 3 é: %d\n", numero % 3);
+
+    return 0;
+}
