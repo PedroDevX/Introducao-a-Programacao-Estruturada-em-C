afb86e6eab3fd5a01c237435056e4a7c022c350a
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:11:16 -0300

    Update exercício3.c

--- c/iniciante/exercício3.c
+++ c/iniciante/exercício3.c
@@ -1,1 +1,9 @@
-
+#include <stdio.h>
+int main() {
+ float n1, n2, media;
+ printf("Digite duas notas: ");
+ scanf("%f %f", &n1, &n2);
+ media = (n1 + n2) / 2;
+ printf("Media: %.2f\n", media);
+ return 0;
+}
