dbac1e83eb0d75406e212ae84969c94a2853b129
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:12:14 -0300

    Update exercício4.c

--- c/iniciante/exercício4.c
+++ c/iniciante/exercício4.c
@@ -1,1 +1,12 @@
-
+#include <stdio.h>
+int main() {
+ int num;
+ printf("Digite um numero: "); 
+ scanf("%d", &num);
+ if (num > 10) {
+ printf("Maior que 10\n");
+ } else {
+ printf("Menor ou igual a 10\n");
+ }
+ return 0;
+} 
