07bc1d9aabe6eeeba3ff57358d52faf8a2d6afb3
Pedro Paulo Riça <142693673+PedroPauloRica@users.noreply.github.com>
2026-03-25 21:14:36 -0300

    Update exercício7.c

--- c/iniciante/exercício7.c
+++ c/iniciante/exercício7.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
 
+int main() {
+    int numero;
+    float resultado;
+
+    printf("Digite um número inteiro: ");
+    scanf("%d", &numero);
+
+    // Conversão de tipo
+    resultado = (float)numero;
+
+    printf("O número em float é: %.2f\n", resultado);
+
+    return 0;
+}
